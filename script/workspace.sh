15e98a109632f26f01478747a3832dfced38c40b
Lars Thorup <lars@zealake.com>
2021-07-05 16:59:45 +0200

    fix: test: error propagation

--- script/workspace.sh
+++ script/workspace.sh
@@ -1,2 +1,3 @@
+set -e # Note: fail script when a command fails
 export DEMO_LIST="BleAppRecorder BleApp"
 export PACKAGE_LIST="react-native-ble-plx-mock-recorder react-native-ble-plx-mock-recorder-mocha-core react-native-ble-plx-mock-recorder-mocha-cli react-native-ble-plx-mock-recorder-mocha-template"
